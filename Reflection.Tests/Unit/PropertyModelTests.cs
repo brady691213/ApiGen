3f3980f9555d423099a41544eaacc13563812387
Brady Kelly <brady691213@outlook.com>
2024-07-12 23:22:43 +0200

    Enough work for one day.

--- Reflection.Tests/Unit/PropertyModelTests.cs
+++ Reflection.Tests/Unit/PropertyModelTests.cs
@@ -6,7 +6,21 @@ namespace Reflection.Tests.Unit;
 public class PropertyModelTests
 {
     [Fact]
-    public void ExplicitNullableGenericDeclarationIsValid()
+    public void NoAliasExplicitNullableGenericIsValid()
+    {
+        var pRef = new PropertyReflector();
+        var genericType = typeof(SuperGenericNoAlias);
+        var nullableProp = genericType.GetProperty("ExplicitNullables");
+
+        var model = pRef.GetPropertyModel(nullableProp, skipAliases:true);
+
+        var dec = model.TypeDeclaration;
+        
+        dec.ShouldBe("List<Nullable<Int32>>?");
+    }
+    
+    [Fact]
+    public void AliasedExplicitNullableGenericIsValid()
     {
         var pRef = new PropertyReflector();
         var genericType = typeof(SuperGenericNoAlias);
@@ -16,11 +30,25 @@ public class PropertyModelTests
 
         var dec = model.TypeDeclaration;
         
+        dec.ShouldBe("List<Nullable<int>>?");
+    }
+    
+    [Fact]
+    public void NoAliasShortFormNullableGenericIsValid()
+    {
+        var pRef = new PropertyReflector();
+        var genericType = typeof(SuperGenericNoAlias);
+        var nullableProp = genericType.GetProperty("ShortFormNullables");
+
+        var model = pRef.GetPropertyModel(nullableProp, skipAliases:true);
+
+        var dec = model.TypeDeclaration;
+        
         dec.ShouldBe("List<Nullable<Int32>>?");
     }
     
     [Fact]
-    public void ShortFormNullableGenericDeclarationIsValid()
+    public void AliasedShortFormNullableGenericIsValid()
     {
         var pRef = new PropertyReflector();
         var genericType = typeof(SuperGenericNoAlias);
@@ -30,6 +58,6 @@ public class PropertyModelTests
 
         var dec = model.TypeDeclaration;
         
-        dec.ShouldBe("List<Nullable<Int32>>?");
+        dec.ShouldBe("List<Nullable<int>>?");
     }
 }
