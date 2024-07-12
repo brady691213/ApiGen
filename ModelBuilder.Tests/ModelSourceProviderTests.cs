4cfc1ad10d6c05fa72e3f3a27491834a95a5f743
Brady Kelly <brady691213@outlook.com>
2024-07-12 17:40:22 +0200

    Some test cleanup

--- ModelBuilder.Tests/ModelSourceProviderTests.cs
+++ ModelBuilder.Tests/ModelSourceProviderTests.cs
@@ -14,27 +14,29 @@ public class ModelSourceProviderTests
     [Fact]
     public void BuildEntityDtoRendersCorrectProps()
     {
-        var _sourceProvider = new ModelSourceProvider();
-        
+        var sourceProvider = new ModelSourceProvider();
         var entityType = typeof(CourseTemplate);
-
-        var _reflector = new DbContextReflector();
-        var expectedProps = _reflector.GetEntityProperties(entityType)
+        var reflector = new DbContextReflector();
+        
+        var expectedDecs = reflector.GetEntityProperties(entityType)
             .Select(p => new PropertyDeclaration(p.PropertyType.Name, p.Name))
             .ToList();
 
-        var actual = _sourceProvider.BuildEntityDto(entityType);
+        var actualCode = sourceProvider.BuildEntityDto(entityType);
+        
+        var actualDecs = GetActualDeclarations(actualCode);
+        actualDecs.ShouldBe(expectedDecs, ignoreOrder: true);
+    }
 
+    private List<PropertyDeclaration> GetActualDeclarations(string actual)
+    {
         var lines = actual.Split('\n');
-
         var actualDecs = new List<PropertyDeclaration>();
-
         for (var i = 1; i < lines.Length - 2; i++)
         {
             var parts = lines[i].Split(' ');
             actualDecs.Add(new PropertyDeclaration(parts[1], parts[2]));
         }
-
-        actualDecs.ShouldBe(expectedProps, ignoreOrder: true);
+        return actualDecs;
     }
 }
