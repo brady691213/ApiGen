77708a5c3e087c588d20f208a1ec8cc34485541f
Brady Kelly <brady691213@outlook.com>
2024-07-14 13:34:11 +0200

    Added stuff for full IHost

---
+++ RepriseCmd/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureHostConfiguration(configHost =>
+    {
+        configHost.SetBasePath(Directory.GetCurrentDirectory());
+        configHost.AddJsonFile("appsettings.json", optional: true);
+    })
+    .ConfigureServices(services =>
+    {
+    })
+    .Build();
