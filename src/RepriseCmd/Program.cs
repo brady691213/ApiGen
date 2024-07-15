25e24b9971ea0b3763034eacb79c57265e8177c1
Brady Kelly <brady691213@outlook.com>
2024-07-15 19:42:42 +0200

    Trimmed out example code

--- src/RepriseCmd/Program.cs
+++ src/RepriseCmd/Program.cs
@@ -10,32 +10,15 @@ using RepriseCmd.Commands;
 
 var executor = CommandExecutor.For(_ =>
 {
-    // Automatically discover and register
-    // all OaktonCommand's in this assembly
     _.RegisterCommands(typeof(Program).GetTypeInfo().Assembly);
         
-    // You can also add commands explicitly from
-    // any assembly
-    _.RegisterCommand<HelloCommand>();
-        
-    // In the absence of a recognized command name,
-    // this is the default command to try to 
-    // fit to the arguments provided
-    _.DefaultCommand = typeof(ColorCommand);
-
-        
+    // TASKT: Arrive at a suitable default command.
+    _.DefaultCommand = typeof(BuildTypeCommand);
+    
     _.ConfigureRun = run =>
     {
-        // you can use this to alter the values
-        // of the inputs or actual command objects
-        // just before the command is executed
+        // you can use this to alter the values of the inputs or actual command objects just before the command is executed
     };
-        
-    // This is strictly for the as yet undocumented
-    // feature in stdocs to generate and embed usage information
-    // about console tools built with Oakton into
-    // stdocs generated documentation websites
-    _.SetAppName("MyApp");
 }, new RepriseCommandCreator(container));
 
 //return await HostProvider.AppStartup().RunOaktonCommands(args);
