a9c786fc0d7673f4079c422fbf72fbb31103e91c
Brady Kelly <brady691213@outlook.com>
2024-07-18 20:49:30 +0200

    Ignore generated outputin test-output

--- src/RepriseCmd/Program.cs
+++ src/RepriseCmd/Program.cs
@@ -6,18 +6,20 @@ using RepriseCmd.Commands;
 
 var appBuilder = new ConsoleAppBuilder();
 
-appBuilder.BuildHelloWorldApp(@"C:\Users\brady\projects\ApiGen\sample-output\");
+//appBuilder.BuildHelloWorldApp(@"C:\Users\brady\projects\ApiGen\sample-output\");
 
-// var host = RepriseHostBuilder.BuildHost();
-// var executor = CommandExecutor.For(_ =>
-// {
-//     _.RegisterCommands(typeof(Program).GetTypeInfo().Assembly);
-//     _.DefaultCommand = typeof(BuildAppCommand);
-// }, new RepriseCommandCreator(host.Services));
-//
-// //return await host.RunOaktonCommands(args);
-// return CommandExecutor.ExecuteCommand<BuildAppCommand>(args);
-// //return executor.Execute(args);
+var host = RepriseHostBuilder.BuildHost();
+var executor = CommandExecutor.For(_ =>
+{
+    _.RegisterCommands(typeof(Program).GetTypeInfo().Assembly);
+    _.DefaultCommand = typeof(BuildAppCommand);
+}, new RepriseCommandCreator(host.Services));
+
+//return await host.RunOaktonCommands(args);
+//return CommandExecutor.ExecuteCommand<BuildAppCommand>(args);
+
+//args = [""]
+return executor.Execute(args);
 
 
     
