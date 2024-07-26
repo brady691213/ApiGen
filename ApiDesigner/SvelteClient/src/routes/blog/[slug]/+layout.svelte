c6dd4e3594272dfad3f7e3b0d4a7864937b5c3ab
Brady Kelly <brady691213@outlook.com>
2024-07-26 05:21:16 +0200

    Sidebar

--- ApiDesigner/SvelteClient/src/routes/blog/[slug]/+layout.svelte
+++ ApiDesigner/SvelteClient/src/routes/blog/[slug]/+layout.svelte
@@ -4,8 +4,19 @@
 
 <div class="layout">
     <main>
-        <slot />
+        <slot></slot>
     </main>
+
+    <aside>
+        <h2>More posts</h2>
+        <ul>
+            {#each data.summaries as { slug, title }}
+                <li>
+                    <a href="/blog/{slug}">{title}</a>
+                </li>
+            {/each}
+        </ul>
+    </aside>
 </div>
 
 <style>
